808453c9847afc606fcfb28ecac505a07eb1e8a3
vadymdota2-rgb <vadymdota2@gmail.com>
2026-09-04 01:06:38 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@
 set -e
 PORT="${PORT:-8080}"
 sed -i "s/listen 8080/listen ${PORT}/" /etc/nginx/conf.d/default.conf
+if [ -n "${API_UPSTREAM:-}" ]; then
+  sed -i "s|proxy_pass http://127.0.0.1:8090;|proxy_pass ${API_UPSTREAM%/};|" /etc/nginx/conf.d/default.conf
+fi
 exec nginx -g "daemon off;"
