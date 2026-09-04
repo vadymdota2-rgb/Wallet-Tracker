12afc71eb1dc055b836bbc143f8c5771d4dbcd60
vadymdota2-rgb <vadymdota2-rgb@users.noreply.github.com>
2026-09-04 14:08:54 +0000

    live bootstrap: hists, quotes, gecko icons, nginx /quotes

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ set -e
 PORT="${PORT:-8080}"
 sed -i "s/listen 8080/listen ${PORT}/" /etc/nginx/conf.d/default.conf
 if [ -n "${API_UPSTREAM:-}" ]; then
-  sed -i "s|proxy_pass http://127.0.0.1:8090;|proxy_pass ${API_UPSTREAM%/};|" /etc/nginx/conf.d/default.conf
+  sed -i "s|http://127.0.0.1:8090|${API_UPSTREAM%/}|g" /etc/nginx/conf.d/default.conf
 fi
 exec nginx -g "daemon off;"
